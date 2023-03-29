a2311f25e4f0178682827e7279e164b8336b4124
AlinaTrukhina <alina.trukhina@gmail.com>
2023-03-29 10:03:49 -0500

    build

---
+++ src/routes/projects/cli-games/+page.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+	<title>Command Line Games</title>
+	<meta name="description" content="Command Line Games" />
+</svelte:head>
+
+<section>
+</section>
--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,18 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		adapter: adapter()
-	}
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	kit: {
+// 		adapter: adapter()
+// 	}
+// };
+
+// export default config;
+
+import adapter from '@sveltejs/adapter-node';
+ 
+export default {
+  kit: {
+    adapter: adapter()
+  }
 };
-
-export default config;
